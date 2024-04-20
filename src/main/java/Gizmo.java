560b9da17a3c5d8f5478b3cc0d171c9dde5ac2e0
ailew <143014764+ailew@users.noreply.github.com>
2024-04-20 03:29:30 -0700

    Update Gizmo.java

--- src/main/java/Gizmo.java
+++ src/main/java/Gizmo.java
@@ -1,35 +1,33 @@
 public class Gizmo
 {
- public class OnlinePurchaseManager {
-    /** An ArrayList of purchased Gizmo objects, instantiated in the constructor. */
-    private ArrayList<Gizmo> purchases;
-
-    /** Returns the number of purchased Gizmo objects that are electronic
-    * whose manufacturer is maker, as described in part (a).
-    */
-    public int countElectronicsByMaker(String maker) {
-        int count = 0;
-        for (Gizmo gizmo : purchases) {
-            if (gizmo.isElectronic() && gizmo.getMaker().equals(maker)) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    /** Returns true if any pair of adjacent purchased Gizmo objects are
-    * equivalent, and false otherwise, as described in part (b).
-    */
-    public boolean hasAdjacentEqualPair() {
-        if (purchases.size() < 2) {
-            return false;
-        }
-        for (int i = 0; i < purchases.size() - 1; i++) {
-            if (purchases.get(i).equals(purchases.get(i + 1))) {
-                return true;
-            }
-        }
-        return false;
-    }
+  private String maker;
+  private boolean isElec;
+  
+  /** Returns the name of the manufacturer of this Gizmo. */
+  public String getMaker()
+  {
+    return maker;
+  }
+  /** Returns true if this Gizmo is electronic, and false
+  * otherwise.
+  */
+  public boolean isElectronic()
+  {
+    return isElec;
+  }
+  /** Returns true if this Gizmo is equivalent to the Gizmo
+  * object represented by the
+  * parameter, and false otherwise.
+  */
+  public boolean equals(Object other)
+  {
+    boolean sameMaker = maker.equals(((Gizmo)other).getMaker());
+    boolean bothElec = isElec == ((Gizmo)other).isElectronic();
+    return sameMaker && bothElec;
+  }
+  public Gizmo(String s, boolean e)
+  {
+    maker = s;
+    isElec = e;
   }
 }
